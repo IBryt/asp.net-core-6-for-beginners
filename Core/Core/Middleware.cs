5c5f9a5b4649eee2f8dcc9f31c675bb0b03be9e1
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-20 11:47:12 +0200

    Defining Middleware using a Class

---
+++ Core/Core/Middleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Core;
+
+public class Middleware
+{
+    private readonly RequestDelegate _next;
+
+    public Middleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.ContentType = "text/plane";
+               
+            }
+            await context.Response.WriteAsync("Class-based Middleware \n");
+        }
+        await _next(context);
+    }
+}
--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,5 +1,9 @@
+using Core;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.UseMiddleware<Middleware>();
 
 app.MapGet("/", () => "Hello World!");
 
