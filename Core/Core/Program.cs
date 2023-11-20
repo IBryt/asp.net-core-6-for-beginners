85984f96094aaa559f2926d567b21be62c2d77ed
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-20 12:08:25 +0200

    Creating Pipeline Branches

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -3,6 +3,14 @@ using Core;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+((IApplicationBuilder)app).Map("/branch", branch =>
+{
+    branch.Use(async (HttpContext context, Func<Task> next) =>
+    {
+        await context.Response.WriteAsync("Branch middleware");
+    });
+});
+
 app.UseMiddleware<Middleware>();
 
 app.MapGet("/", () => "Hello World!");
