c193de74deb1563659943b7a0f1f7686ff82d654
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-21 11:15:40 +0200

    Using Cookies

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,30 +1,29 @@
-using Core;
-
 var builder = WebApplication.CreateBuilder(args);
 
-var servicesConfig = builder.Configuration;
-builder.Services.Configure<FruitOptions>(servicesConfig.GetSection("Fruit"));
 var app = builder.Build();
 
-app.Logger.LogDebug("Pipeline configuration starting");
-app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
-{
-    string defaultDebug = config["Logging:LogLevel:Default"];
-    await context.Response.WriteAsync(defaultDebug);
 
-    string environment = config["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync(environment);
-    if (app.Environment.IsDevelopment())
-    {
-        await context.Response.WriteAsync("IsDevelopment");
-    }
+app.MapGet("/cookie", async context =>
+{
+    int counter = int.Parse(context.Request.Cookies["counter"] ?? "0") + 1;
+    context.Response.Cookies.Append(
+            "counter",
+            counter.ToString(),
+            new CookieOptions
+            {
+                MaxAge = TimeSpan.FromMinutes(30)
+            }
+    );
+    await context.Response.WriteAsync($"Cookie: {counter}");
 });
 
-app.UseMiddleware<FruitMiddleware>();
+app.MapGet("/clear", context =>
+{
+    context.Response.Cookies.Delete("counter");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
 
 app.MapGet("/", () => "Hello World!");
-
-app.Logger.LogDebug("Pipeline configuration complete");
-app.UseStaticFiles();
 
 app.Run();
