9b14021edf23800d48b1bb6cfda19960d39ba0aa
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-21 15:35:51 +0200

    Enabling HTTP Strict Transport Security (HSTS)

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,12 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHsts((options) =>
+{
+    options.MaxAge = TimeSpan.FromDays(1);
+    options.IncludeSubDomains = true;
+});
+
 var app = builder.Build();
+
 app.MapGet("/https", async (context) =>
 {
     await context.Response.WriteAsync($"Http request: {context.Request.IsHttps}");
 });
 
 app.UseHttpsRedirection();
+
+if (app.Environment.IsProduction())
+{ 
+    app.UseHsts();
+}
 
 app.MapGet("/", () => "Hello World!");
 
