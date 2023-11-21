5867bf7e0ffb2cb543f0860f8d5c935d2361fb1e
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-21 08:59:09 +0200

    Using Configuration Data with the Options Pattern

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,5 +1,25 @@
+using Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var servicesConfig = builder.Configuration;
+builder.Services.Configure<FruitOptions>(servicesConfig.GetSection("Fruit"));
 var app = builder.Build();
+
+app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
+{
+    string defaultDebug = config["Logging:LogLevel:Default"];
+    await context.Response.WriteAsync(defaultDebug);
+
+    string environment = config["ASPNETCORE_ENVIRONMENT"];
+    await context.Response.WriteAsync(environment);
+    if (app.Environment.IsDevelopment())
+    {
+        await context.Response.WriteAsync("IsDevelopment");
+    }
+});
+
+app.UseMiddleware<FruitMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
