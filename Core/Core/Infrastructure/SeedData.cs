40c0ba792263111147842706e8379be38281d201
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-22 09:34:34 +0200

    Adding Seed Data

---
+++ Core/Core/Infrastructure/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Infrastructure;
+
+public class SeedData
+{
+    public static void SeedDatabase(DataContext context)
+    {
+        context.Database.Migrate();
+
+        if (context.Products.Count() == 0 && context.Categories.Count() == 0)
+        {
+            Category fruits = new Category { Name = "fruits" };
+            Category shirts = new Category { Name = "shirts" };
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Apple",
+                    Price = 1.5M,
+                    Category = fruits,
+                },
+                new Product
+                {
+                    Name = "Lemon",
+                    Price = 2M,
+                    Category = fruits,
+                },
+                new Product
+                {
+                    Name = "Watermelon",
+                    Price = 0.5M,
+                    Category = fruits,
+                },
+                new Product
+                {
+                    Name = "Grapefruit",
+                    Price = 2.5M,
+                    Category = fruits,
+                },
+                new Product
+                {
+                    Name = "Blue shirt",
+                    Price = 7.99M,
+                    Category = shirts,
+                },
+                new Product
+                {
+                    Name = "Black shirt",
+                    Price = 5.99M,
+                    Category = shirts,
+                },
+                new Product
+                {
+                    Name = "Red shirt",
+                    Price = 9.99M,
+                    Category = shirts,
+                },
+                new Product
+                {
+                    Name = "Yellow shirt",
+                    Price = 11.99M,
+                    Category = shirts,
+                }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -8,26 +8,9 @@ builder.Services.AddDbContext<DataContext>((options) =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
 });
 
-
-builder.Services.AddHsts((options) =>
-{
-    options.MaxAge = TimeSpan.FromDays(1);
-    options.IncludeSubDomains = true;
-});
-
 var app = builder.Build();
-
-app.MapGet("/https", async (context) =>
-{
-    await context.Response.WriteAsync($"Http request: {context.Request.IsHttps}");
-});
-
-app.UseHttpsRedirection();
-
-if (app.Environment.IsProduction())
-{
-    app.UseHsts();
-}
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
 
 app.MapGet("/", () => "Hello World!");
 
