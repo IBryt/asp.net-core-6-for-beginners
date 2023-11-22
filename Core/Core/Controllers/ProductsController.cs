60702db205cc562fe0993bf177aae360b4f142e9
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-22 16:02:56 +0200

    Creating CRUD controller

--- Core/Core/Controllers/ProductsController.cs
+++ Core/Core/Controllers/ProductsController.cs
@@ -15,22 +15,49 @@ public class ProductsController : Controller
     }
 
     [HttpGet]
-    public IEnumerable<Product> GetProducts()
+    public IAsyncEnumerable<Product> GetProducts()
     {
-        return  _dataContext.Products;
+        return _dataContext.Products.AsAsyncEnumerable();
     }
 
     [HttpGet("{id}")]
-    public Product GetProductById(long id,[FromServices]ILogger<ProductsController> logger)
+    public async Task<IActionResult> GetProductById(long id, [FromServices] ILogger<ProductsController> logger)
     {
         logger.LogDebug("GetProductById Action Invoked");
-        return _dataContext.Products.Find(id);
+        var product = await _dataContext.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
     }
 
     [HttpPost]
-    public void  SaveProduct([FromBody] Product product)
+    public async Task<IActionResult> SaveProduct([FromBody] Product product)
     {
-        _dataContext.Products.Add(product);
+        await _dataContext.Products.AddAsync(product);
+        await _dataContext.SaveChangesAsync();
+        return Ok(product);
+    }
+
+    [HttpPut]
+    public void UpdateProduct([FromBody] Product product)
+    {
+        _dataContext.Update(product);
         _dataContext.SaveChanges();
+    }
+
+    [HttpDelete("{id}")]
+    public void DeleteProduct(long id)
+    {
+        _dataContext.Products.Remove(new Product { Id = id });
+        _dataContext.SaveChanges();
+    }
+
+    [HttpGet("redirect")]
+    public IActionResult Redirect()
+    {
+        return RedirectToAction(nameof(GetProductById), "Products", new { Id = 1 });
     }
 }
