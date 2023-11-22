f034d4cdeed8b5891b7d9eb8be0bf9e07ffbe97e
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-22 10:35:35 +0200

    Creating a Simple Web Service

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,5 +1,7 @@
 using Core.Infrastructure;
+using Core.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,53 @@ builder.Services.AddDbContext<DataContext>((options) =>
 });
 
 var app = builder.Build();
+
+const string BASEURL = "/api/products";
+
+app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
+{
+    string? id = context.Request.RouteValues["id"] as string;
+
+    if (string.IsNullOrEmpty(id))
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        return;
+    }
+
+    Product? product = data.Products.Find(long.Parse(id));
+
+    if (product == null)
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        return;
+    }
+
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+});
+
+app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
+{
+    IEnumerable<Product> products = data.Products.AsEnumerable();
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync(JsonSerializer.Serialize(products));
+});
+
+app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
+{
+    Product? product = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+
+    if (product == null) 
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return;
+    }
+
+    await data.AddAsync(product);
+    await data.SaveChangesAsync();
+    context.Response.StatusCode = StatusCodes.Status200OK;
+});
+
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
 
