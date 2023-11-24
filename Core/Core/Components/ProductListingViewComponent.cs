118fb68b4b774c4516a0f16f12089af1d365acbc
IBryt <36583283+IBryt@users.noreply.github.com>
2023-11-24 11:47:16 +0200

    View Components

---
+++ Core/Core/Components/ProductListingViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Core.Infrastructure;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Components;
+
+public class ProductListingViewComponent : ViewComponent
+{
+    private DataContext _dataContext;
+
+    public IEnumerable<Product> Products;
+
+    public ProductListingViewComponent(DataContext context)
+    {
+        _dataContext = context;
+    }
+
+    public string Invoke()
+    {
+        return $"There are {_dataContext.Products.Count()} products";
+    }
+}
--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -1,41 +1,24 @@
 using Core.Infrastructure;
-using Core.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DataContext>((options) =>
+builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
 });
 
-builder.Services.AddControllersWithViews().AddNewtonsoftJson();
-
-builder.Services.Configure<MvcNewtonsoftJsonOptions>((options) =>
-{
-    options.SerializerSettings.NullValueHandling = 
-        Newtonsoft.Json.NullValueHandling.Ignore;
-});
-
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapRazorPages();
-
 app.MapControllers();
-//app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+
 app.MapDefaultControllerRoute();
+
 app.UseStaticFiles();
+
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
 
-//app.MapGet("/", () => "Hello World!");
-
 app.Run();
-
-//migration commands
-//dotnet ef migrations add Initial
-//dotnet ef database update 
