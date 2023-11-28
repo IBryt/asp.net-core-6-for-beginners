536221339e03b5fe4b8be9c209284a659805bde0
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2023-11-28 15:40:44 +0200

    Inserting Content Inside the Output Element

---
+++ Core/Core/TagHelpers/HighLightTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Core.TagHelpers;
+
+[HtmlTargetElement("*", Attributes = "[highlight=true]")]
+public class HighLightTagHelper : TagHelper
+{
+    public string WrapperString { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.PreContent.SetHtmlContent("<b><i>");
+        output.PostContent.SetHtmlContent("</i></b>");
+    }
+}
