289a4b6073870ab9a1732780b39b79aff4382b01
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2023-11-30 12:21:59 +0200

    Adding Identity

---
+++ Core/Core/Infrastructure/IdentityContext.cs
@@ -0,0 +1,11 @@
+﻿using Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Infrastructure;
+
+public class IdentityContext : IdentityDbContext<IdentityUser>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+}
--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -9,6 +9,15 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
 });
 
+builder.Services.AddDbContext<IdentityContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -19,7 +28,16 @@ app.MapDefaultControllerRoute();
 
 app.UseStaticFiles();
 
+app.UseAuthorization();
+
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
 
 app.Run();
+
+/*
+ * Console command
+ * cd Core
+ * dotnet ef migrations add --context IdentityContext Initial
+ * dotnet ef database update --context IdentityContext
+ */
